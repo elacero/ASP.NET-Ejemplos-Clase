40b35685eb8da769acaf315f9acda137133fcf1d
elacero <aiv@ieslasfuentezuelas.com>
2016-01-15 20:26:11 +0100

    Primer commit con el trabajo de clase del día 15-01-2016

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PrimerosPasos.Models;
+
+namespace PrimerosPasos.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private ProductoDBContext db = new ProductoDBContext();
+
+        // GET: Productos
+        public ActionResult Index()
+        {
+            return View(db.Productos.ToList());
+        }
+
+        // GET: Productos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // GET: Productos/Create
+        public ActionResult Create()
+        {
+            //Producto nuevo = new Producto();
+            //nuevo.Descripcion = "Caramero de menta";
+            //nuevo.Nombre = "Caramelo";
+            //nuevo.Precio = 25;
+            var productosmayor10 = db.Productos.Select(p => p.Precio > 10);
+            
+            return View();
+        }
+
+        // POST: Productos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Nombre,Descripcion,Precio")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Productos.Add(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(producto);
+        }
+
+        // GET: Productos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: Productos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nombre,Descripcion,Precio")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(producto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(producto);
+        }
+
+        // GET: Productos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: Productos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Producto producto = db.Productos.Find(id);
+            db.Productos.Remove(producto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PrimerosPasos.Models
+{
+    public class Producto
+    {
+        public int ID { get; set; }
+        public String Nombre { get; set; }
+        public String Descripcion {get;set;}
+        public decimal Precio { get; set; }
+    }
+
+    public class ProductoDBContext:DbContext
+    {
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
