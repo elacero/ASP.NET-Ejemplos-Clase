e00398fdf2373e62948e0798f1bccdb618509dc9
elacero <aiv@ieslasfuentezuelas.com>
2016-01-17 19:35:50 +0100

    Trabajo de clase 15-01-2016

---
+++ PrimerosPasos/Controllers/HolaMundoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrimerosPasos.Controllers
+{
+    public class HolaMundoController : Controller
+    {
+        // GET: HolaMundo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+
+    }
+}
---
+++ PrimerosPasos/Controllers/MiControladorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PrimerosPasos.Models;
+
+namespace PrimerosPasos.Controllers
+{
+    public class MiControladorController : Controller
+    {
+        private ProductoDBContext contexto = new ProductoDBContext();
+
+        // GET: MiControlador
+        public ActionResult Index()
+        {
+            ViewData["productos"] = contexto.Productos;
+            return View();
+        }
+
+        public ActionResult Crear()
+        {
+           
+            return View();
+        }
+    }
+}
